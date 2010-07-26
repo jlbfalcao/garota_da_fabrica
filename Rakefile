45151e65dacb67ccda486a6e9e0020a1b532a652
Jorge Falcão <jlbfalcao@gmail.com>
2010-07-26 10:38:34 -0300

    primeira versão do garota_da_fabrica

--- Rakefile
+++ Rakefile
@@ -5,13 +5,12 @@ begin
   require 'jeweler'
   Jeweler::Tasks.new do |gem|
     gem.name = "garota_da_fabrica"
-    gem.summary = %Q{TODO: one-line summary of your gem}
-    gem.description = %Q{TODO: longer description of your gem}
+    gem.summary = %Q{Cucumber steps em português para FactoryGirl}
+    gem.description = %Q{Utilize os "steps_definitions" do FactoryGirl em português com o Cucumber.}
     gem.email = "jlbfalcao@gmail.com"
     gem.homepage = "http://github.com/jlbfalcao/garota_da_fabrica"
     gem.authors = ["Jorge Falcão"]
-    gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
-    # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
+    gem.add_dependency "factory-girl"
   end
   Jeweler::GemcutterTasks.new
 rescue LoadError
---
+++ lib/garota_da_fabrica.rb
@@ -0,0 +1,48 @@
+
+# Tradução dos steps da framework factory girl 
+
+FactoryGirl.factories.values.each do |factory|
+  Dado /^que existem? (?:o|a)s? seguintes? (?:#{factory.human_name}|#{factory.human_name.pluralize}):$/ do |table|
+    # [Sergio] Se precisar debugar o step eh soh descomentar isso aqui:
+    # begin
+    Given %{the following #{factory.human_name} exists:}, table
+    # rescue ActiveRecord::RecordInvalid => err
+    #     err.record.errors.each do |e,t|
+    #       puts "#{factory.human_name}"
+    #       puts "#{e} - #{t}"
+    #       puts err.record.class
+    #       breakpoint  
+    #       0
+    #     end
+    # end    
+  end
+  
+  Dado /^que existe uma? #{factory.human_name}$/ do
+    Given %{an #{factory.human_name} exists}
+  end
+
+  Dado /^que existem (\d+) #{factory.human_name.pluralize}$/ do |count|
+    Given %{#{count} #{factory.human_name.pluralize} exist}
+  end
+
+  # puts %{que existem? (?:o|a)s? seguintes? #{factory.human_name}|#{factory.human_name.pluralize}:}
+  # puts %{que existe uma? #{factory.human_name}}
+  # puts %{que existem (\d+) #{factory.human_name.pluralize}}
+
+ if factory.build_class.respond_to?(:columns)
+    factory.build_class.columns.each do |column|
+      human_column_name = column.name.downcase.gsub('_', ' ')
+      #   
+      # puts %{que existe uma? #{factory.human_name} com #{human_column_name} igual a "([^"]*)"}
+      # puts %{que existem (\d+) #{factory.human_name.pluralize} com #{human_column_name} igual a "([^"]*)"}
+  
+      Dado /^que existe uma? #{factory.human_name} com #{human_column_name} igual a "([^"]*)"$/ do |value|
+        Given %{an #{factory.human_name} exists with an #{human_column_name} of "#{value}"}
+      end
+
+      Dado /^que existem (\d+) #{factory.human_name.pluralize} com #{human_column_name} igual a "([^"]*)"$/ do |count, value|
+        Given %{#{count} #{factory.human_name.pluralize} exist with an #{human_column_name} of "#{value}"}
+      end
+    end
+  end
+end
